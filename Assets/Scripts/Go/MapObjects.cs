3132cbcc4818f316b6eefa467e346d608b7375bb
tomual <tomual@users.noreply.github.com>
2020-02-10 16:02:36 +0800

    Set sprite renderer sorting order depending on Y position

---
+++ Assets/Scripts/Go/MapObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+public class MapObjects : MonoBehaviour
+{
+    void Start()
+    {
+        int layer = (int)(transform.position.y * 10);
+        layer *= 100;
+
+        SpriteRenderer spriteRenderers = GetComponent<SpriteRenderer>();
+        int baseLayer = spriteRenderers.sortingOrder % 100;
+        spriteRenderers.sortingOrder = baseLayer + layer * -1;
+    }
+}
--- Assets/Scripts/Go/Player.cs
+++ Assets/Scripts/Go/Player.cs
@@ -11,39 +11,47 @@ public class Player : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        string[] toDefault =
-        {
-            "arm.l",
-            "arm.r",
-            "leg.l",
-            "leg.r",
-            "torso",
-            "head",
-            "head.face",
-        };
-        foreach (Transform child in transform)
-        {
-            Debug.Log(child.name);
-            SpriteResolver spriteResolver = child.GetComponent<SpriteResolver>();
-            if (spriteResolver)
-            {
-                if (Array.IndexOf(toDefault, child.name) >= 0)
-                {
-                    spriteResolver.SetCategoryAndLabel(child.name, "default");
-                }
-                else
-                {
-                    spriteResolver.SetCategoryAndLabel(child.name, "empty");
-                }
-            }
-        }
+        //string[] toDefault =
+        //{
+        //    "arm.l",
+        //    "arm.r",
+        //    "leg.l",
+        //    "leg.r",
+        //    "torso",
+        //    "head",
+        //    "head.face",
+        //};
+        //foreach (Transform child in transform)
+        //{
+        //    Debug.Log(child.name);
+        //    SpriteResolver spriteResolver = child.GetComponent<SpriteResolver>();
+        //    if (spriteResolver)
+        //    {
+        //        if (Array.IndexOf(toDefault, child.name) >= 0)
+        //        {
+        //            spriteResolver.SetCategoryAndLabel(child.name, "default");
+        //        }
+        //        else
+        //        {
+        //            spriteResolver.SetCategoryAndLabel(child.name, "empty");
+        //        }
+        //    }
+        //}
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int layer = (int) (transform.position.y * 10);
+        layer *= 100;
+
+        SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < spriteRenderers.Length; i++)
+        {
+            int baseLayer = spriteRenderers[i].sortingOrder % 100;
+            spriteRenderers[i].sortingOrder = baseLayer + layer * -1;
+        }
     }
 
     private void FixedUpdate()
@@ -54,7 +62,7 @@ public class Player : MonoBehaviour
         if (horizontal != 0 || vertical != 0)
         {
             Vector3 target = transform.position + new Vector3(horizontal, vertical);
-            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * 3);
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * 4);
             if (horizontal < 0)
             {
                 transform.localScale = new Vector3(1, 1);
