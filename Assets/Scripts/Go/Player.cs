acf32dd4092f4dba4f4bf1ac3ce5057bf58b8774
tomual <tomual@users.noreply.github.com>
2020-02-10 18:55:55 +0800

    HP/MP frames

--- Assets/Scripts/Go/Player.cs
+++ Assets/Scripts/Go/Player.cs
@@ -3,47 +3,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.U2D.Animation;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     Animator animator;
-    // Start is called before the first frame update
-    void Start()
+
+    public int hp;
+    public int mp;
+    public int maxHp;
+    public int maxMp;
+
+    void Awake()
     {
         animator = GetComponent<Animator>();
-        //string[] toDefault =
-        //{
-        //    "arm.l",
-        //    "arm.r",
-        //    "leg.l",
-        //    "leg.r",
-        //    "torso",
-        //    "head",
-        //    "head.face",
-        //};
-        //foreach (Transform child in transform)
-        //{
-        //    Debug.Log(child.name);
-        //    SpriteResolver spriteResolver = child.GetComponent<SpriteResolver>();
-        //    if (spriteResolver)
-        //    {
-        //        if (Array.IndexOf(toDefault, child.name) >= 0)
-        //        {
-        //            spriteResolver.SetCategoryAndLabel(child.name, "default");
-        //        }
-        //        else
-        //        {
-        //            spriteResolver.SetCategoryAndLabel(child.name, "empty");
-        //        }
-        //    }
-        //}
 
+        InitStats();
+        InitSpriteResolve();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        int layer = (int) (transform.position.y * 10);
+        UpdateSpriteSortingLayer();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Portal portal = collision.GetComponent<Portal>();
+            if (portal && !string.IsNullOrEmpty(portal.destination))
+            {
+                UIManager.instance.Portal(portal.destination);
+            }
+        }
+    }
+
+    private void InitSpriteResolve()
+    {
+        string[] toDefault =
+        {
+            "arm.l",
+            "arm.r",
+            "leg.l",
+            "leg.r",
+            "torso",
+            "head",
+            "head.face",
+        };
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.name);
+            SpriteResolver spriteResolver = child.GetComponent<SpriteResolver>();
+            if (spriteResolver)
+            {
+                if (Array.IndexOf(toDefault, child.name) >= 0)
+                {
+                    spriteResolver.SetCategoryAndLabel(child.name, "default");
+                }
+                else
+                {
+                    spriteResolver.SetCategoryAndLabel(child.name, "empty");
+                }
+            }
+        }
+    }
+
+    private void InitStats()
+    {
+        hp = 96;
+        mp = 100;
+        maxHp = 100;
+        maxMp = 100;
+        UIManager.instance.InitHP(maxHp);
+        UIManager.instance.InitMP(maxMp);
+        UIManager.instance.UpdateHP(hp);
+        UIManager.instance.UpdateMP(mp);
+    }
+
+    private void UpdateSpriteSortingLayer()
+    {
+        int layer = (int)(transform.position.y * 10);
         layer *= 100;
 
         SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
@@ -54,7 +99,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void UpdateMovement()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
---
+++ Assets/Scripts/Go/UIManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance = null;
+
+    bool fadeIn = false;
+    bool fadeOut = false;
+
+    CanvasGroup fadeScreen;
+
+    string portalDestination;
+
+    Slider hp;
+    Slider mp;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        fadeScreen = GameObject.Find("FadeScreen").GetComponent<CanvasGroup>();
+        StartCoroutine(DelayEnter());
+
+        hp = GameObject.Find("SliderHP").GetComponent<Slider>();
+        mp = GameObject.Find("SliderMP").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        CheckFadeIn();
+        CheckFadeOut();
+    }
+
+    void CheckFadeIn()
+    {
+        if (fadeIn)
+        {
+            fadeScreen.alpha += 0.05f;
+            if (fadeScreen.alpha >= 1)
+            {
+                fadeScreen.alpha = 1;
+                fadeIn = false;
+            }
+        }
+    }
+
+    void CheckFadeOut()
+    {
+        if (fadeOut)
+        {
+            fadeScreen.alpha -= 0.05f;
+            if (fadeScreen.alpha <= 0)
+            {
+                fadeScreen.alpha = 0;
+                fadeOut = false;
+            }
+        }
+    }
+
+    public void Portal(string destination)
+    {
+        portalDestination = destination;
+        StartCoroutine(DelayPortal());
+    }
+
+    IEnumerator DelayEnter()
+    {
+        yield return new WaitForSeconds(0.2f);
+        fadeOut = true;
+    }
+
+    IEnumerator DelayPortal()
+    {
+        fadeIn = true;
+        yield return new WaitForSeconds(0.4f);
+        SceneManager.LoadScene(portalDestination);
+        yield return new WaitForSeconds(0.2f);
+        fadeOut = true;
+    }
+
+    public void InitHP(int maxHp)
+    {
+        hp.maxValue = maxHp;
+    }
+
+    public void InitMP(int maxMp)
+    {
+        mp.maxValue = maxMp;
+    }
+
+    public void UpdateHP(int value)
+    {
+        hp.value = value;
+    }
+
+    public void UpdateMP(int value)
+    {
+        mp.value = value;
+    }
+}
