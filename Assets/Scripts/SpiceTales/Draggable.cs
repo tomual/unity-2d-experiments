5fe7f56942e184d86fbdf51f545ee53335b6fc12
tomual <tomual@users.noreply.github.com>
2020-05-09 23:36:23 +0800

    Draggable panel window

---
+++ Assets/Scripts/SpiceTales/Draggable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : EventTrigger
+{
+
+    private bool dragging;
+    float offsetX;
+    float offsetY;
+
+    public void Update()
+    {
+        if (dragging)
+        {
+            transform.parent.transform.position = new Vector3(Input.mousePosition.x - offsetX, Input.mousePosition.y - offsetY);
+        }
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        dragging = true;
+
+        Vector3 offset = Input.mousePosition - transform.parent.transform.position;
+        offsetX = offset.x;
+        offsetY = offset.y;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        dragging = false;
+    }
+}
